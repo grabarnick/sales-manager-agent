ef2d22901d69c932c95f1195020d063126902a9f
grabarnick <agrabarnick@gmail.com>
2026-03-26 15:38:59 +0200

    - col

--- project/collections/amoCRM/createLead/code.js
+++
@@ -1,8 +0,0 @@
-return Credentials.get('amocrm_oauth').then(function(auth) {
-  const baseUrl = `https://${auth.name}.amocrm.ru/api/v4`;
-  return Http.post({
-    url: `${baseUrl}/leads`,
-    body: [{ name: name, price: price || 0 }],
-    headers: { Authorization: `Bearer ${auth.token}` }
-  }).then(res => JSON.parse(res.body)._embedded?.leads[0] || null);
-});
--- project/collections/amoCRM/searchContact/code.js
+++
@@ -1,8 +0,0 @@
-return Credentials.get('amocrm_oauth').then(function(auth) {
-  const baseUrl = `https://${auth.name}.amocrm.ru/api/v4`;
-  return Http.get({
-    url: `${baseUrl}/contacts`,
-    params: { query: query },
-    headers: { Authorization: `Bearer ${auth.token}` }
-  }).then(res => JSON.parse(res.body)._embedded?.contacts || []);
-});
